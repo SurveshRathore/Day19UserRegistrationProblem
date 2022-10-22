e07cb6f0bc3fddcb327f02a79dcc0c8c0998079b
SurveshRathore <educatedevilboy@gmail.com>
2022-10-22 08:38:25 +0530

    Validate the first name

---
+++ UC1ValidateFirstName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19UserRegistrationProblem
+{
+    internal class UC1ValidateFirstName
+    {
+        public void CheckFirstName()
+        {
+            String nameRegex = "^[A-Z][a-zA-Z]{2,}"; 
+            Console.Write("Enter the First Name: ");
+            String userName = Console.ReadLine();
+
+            bool valid = Regex.IsMatch(userName, nameRegex);
+            Console.WriteLine(valid);
+        }
+    }
+}
